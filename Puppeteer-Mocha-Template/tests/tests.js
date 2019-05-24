c99cc0df1b35274a9982e9c1e177c674b5eb4dde
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-24 18:09:25 +0100

    Mocha Tests

---
+++ Puppeteer-Mocha-Template/tests/tests.js
@@ -0,0 +1,34 @@
+import assert from "assert";
+import puppeteer from "puppeteer";
+import GoogleSearchPage from "../pages/googlesearchpage";
+import SearchResultsPage from "../pages/searchresultspage";
+
+let browser
+let page
+let googlesearchpage
+let searchresultspage
+
+before(async () => {
+    browser = await puppeteer.launch()
+    page = await browser.newPage()
+    googlesearchpage = new GoogleSearchPage(page)
+    searchresultspage = new SearchResultsPage(page)
+});
+
+describe('Google Test', () => {
+    it('Assert title "Google"', async () => {
+        await googlesearchpage.open()
+        const title = await googlesearchpage.getTitle()
+        assert.equal(title, 'Google')
+    });
+
+    it('Search for "Selenium"', async () => {
+        await googlesearchpage.searchFor('Selenium')
+        assert.equal(await searchresultspage.isSeleniumPresent(), 'Selenium - Web Browser Automation')
+    });
+
+});
+
+after(async () => {
+    await browser.close()
+});
