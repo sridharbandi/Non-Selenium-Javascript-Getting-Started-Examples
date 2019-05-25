ef00b0da5575f9242bf61a712def6dc58203a445
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-25 19:40:38 +0100

    Step Definitions File

---
+++ Puppeteer-Cucumber-Template/steps/steps.js
@@ -0,0 +1,30 @@
+import { Given, When, Then, After, Before } from 'cucumber';
+import puppeteer from "puppeteer";
+import GoogleSearchPage from '../pages/googlesearchpage';
+import SearchResultsPage from '../pages/searchresultspage';
+
+let browser
+let page
+let googlesearchpage;
+let searchresultspage;
+
+Before(async () => {
+    browser = await puppeteer.launch()
+    page = await browser.newPage()
+    googlesearchpage = new GoogleSearchPage(page)
+    searchresultspage = new SearchResultsPage(page)
+});
+
+Given(/^User is on Google search page$/, async () => {
+    await googlesearchpage.open()
+});
+
+When(/^User searches for Selenium$/, async () => {
+    await googlesearchpage.searchFor('Selenium')
+});
+
+Then(/^User can see Selenium results$/, async () => {
+    expect(await searchresultspage.isSeleniumPresent()).toContain('Selenium')
+});
+
+After(() => browser.close());
