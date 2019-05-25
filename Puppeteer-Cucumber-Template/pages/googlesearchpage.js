614bf11f6a51ebe46b822886172f42312612d973
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-25 19:40:04 +0100

    Google Search Page Object

---
+++ Puppeteer-Cucumber-Template/pages/googlesearchpage.js
@@ -0,0 +1,21 @@
+export default class GoogleSearchPage {
+
+    constructor(page) {
+        this.page = page;
+        this.SEARCH_BOX = 'input[name=q]';
+    }
+
+    async open(){
+       return this.page.goto('https://google.com', { waitUntil: 'networkidle2' })
+    }
+
+    async getTitle() {
+        return this.page.title();
+    }
+
+    async searchFor(searchterm){
+        await this.page.waitFor(this.SEARCH_BOX);
+        await this.page.type(this.SEARCH_BOX, searchterm, { delay: 100 });
+        await this.page.keyboard.press('Enter');
+    }
+}
