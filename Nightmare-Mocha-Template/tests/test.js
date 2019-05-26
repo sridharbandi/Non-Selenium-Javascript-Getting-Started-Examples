0378c2cd421d58a545e466fadab3f1e8a9e2709c
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-26 16:58:38 +0100

    Test Scripts

---
+++ Nightmare-Mocha-Template/tests/test.js
@@ -0,0 +1,32 @@
+import Nightmare from "nightmare";
+import {expect} from "chai";
+import GoogleSearchPage from '../pages/googlesearchpage';
+import SearchResultsPage from "../pages/searchresultspage";
+
+describe('Google Search', function () {
+    let nightmare
+    let googlesearchpage
+    let searchresultspage
+    this.timeout(50000)
+
+    before(async () => {
+        nightmare = Nightmare()
+        googlesearchpage = new GoogleSearchPage(nightmare)
+        searchresultspage = new SearchResultsPage(nightmare)
+    });
+
+    it('Assert title "Google"', async () => {
+        await googlesearchpage.open()
+        const title = await googlesearchpage.getTitle()
+        expect(title).to.equal('Google')
+    });
+
+    it('Search for "Selenium"', async function () {
+        await googlesearchpage.searchFor('Selenium')
+        const linkselenium = await searchresultspage.getSeleniumText()
+        expect(linkselenium).to.equal('Selenium - Web Browser Automation')
+    });
+
+    after(() => nightmare.end());
+
+});
