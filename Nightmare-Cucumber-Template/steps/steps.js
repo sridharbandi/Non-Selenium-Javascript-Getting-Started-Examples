f73071ddab322f55d2cafd7ab574e2f96dde3670
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-27 18:37:32 +0100

    Step Definitions

---
+++ Nightmare-Cucumber-Template/steps/steps.js
@@ -0,0 +1,30 @@
+import { Given, When, Then, After, Before } from 'cucumber';
+import Nightmare from "nightmare";
+import {expect} from "chai";
+import GoogleSearchPage from '../pages/googlesearchpage';
+import SearchResultsPage from "../pages/searchresultspage";
+
+let nightmare
+let googlesearchpage
+let searchresultspage
+
+Before(async () => {
+    nightmare = Nightmare()
+    googlesearchpage = new GoogleSearchPage(nightmare)
+    searchresultspage = new SearchResultsPage(nightmare)
+});
+
+Given(/^User is on Google search page$/, async () => {
+    await googlesearchpage.open()
+});
+
+When(/^User searches for Selenium$/, async () => {
+    await googlesearchpage.searchFor('Selenium')
+});
+
+Then(/^User can see Selenium results$/, async () => {
+    const linkselenium = await searchresultspage.getSeleniumText()
+        expect(linkselenium).to.equal('Selenium - Web Browser Automation')
+});
+
+After(() => nightmare.end());
