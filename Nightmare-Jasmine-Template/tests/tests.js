eb3271f01501191749a6ad4688b0c163d336ff1d
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-27 18:36:36 +0100

    Test Scripts

---
+++ Nightmare-Jasmine-Template/tests/tests.js
@@ -0,0 +1,31 @@
+import Nightmare from "nightmare";
+import {expect} from "chai";
+import GoogleSearchPage from '../pages/googlesearchpage';
+import SearchResultsPage from "../pages/searchresultspage";
+
+describe('Google Search', function () {
+    let nightmare
+    let googlesearchpage
+    let searchresultspage
+
+    beforeAll(async () => {
+        nightmare = Nightmare()
+        googlesearchpage = new GoogleSearchPage(nightmare)
+        searchresultspage = new SearchResultsPage(nightmare)
+    });
+
+    it('Assert title "Google"', async () => {
+        await googlesearchpage.open()
+        const title = await googlesearchpage.getTitle()
+        expect(title).to.equal('Google')
+    });
+
+    it('Search for "Selenium"', async function () {
+        await googlesearchpage.searchFor('Selenium')
+        const linkselenium = await searchresultspage.getSeleniumText()
+        expect(linkselenium).to.equal('Selenium - Web Browser Automation')
+    });
+
+    afterAll(() => nightmare.end());
+
+});
