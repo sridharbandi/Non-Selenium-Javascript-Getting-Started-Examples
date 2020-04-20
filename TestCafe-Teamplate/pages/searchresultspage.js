362ef78d9b924f24d289ecb969d88718177d4048
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-04-20 21:50:00 +0200

    Updated Locators

--- TestCafe-Teamplate/pages/searchresultspage.js
+++ TestCafe-Teamplate/pages/searchresultspage.js
@@ -1,12 +1,18 @@
-import { Selector, t } from 'testcafe';
+import { Selector, ClientFunction, t } from 'testcafe';
 
 export default class SearchResultsPage {
 
     constructor() {
-        this.linkselenium = Selector('a[href=\'https://www.seleniumhq.org/\'] > h3');
+        this.linkselenium = Selector('a[href=\'https://www.selenium.dev/\'] > h3');
     }
 
     async getSeleniumText(){
         return this.linkselenium.textContent
+    }
+
+    async executeScript(){
+        const getPageUrl = ClientFunction(() => window.location.href);
+        console.log(await getPageUrl());
+        
     }
 }
